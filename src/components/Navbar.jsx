3708cbf6d15662e9cd11d0bac710607c7e5c59ce
Ghadeer Alkhathlan <g.alkhathlan@gmail.com>
2019-01-27 05:18:35 +0300

    add bootstrap

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Navbar extends Component {
+  constructor() {
+    super();
+    this.state = {  }
+  }
+  render() { 
+    return (  );
+  }
+}
+ 
+export default Navbar ;
